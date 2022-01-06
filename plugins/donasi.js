5711b45bdf61fca52872d8d28b8fedba7b0ae278
Botwa021 <97183746+Botwa021@users.noreply.github.com>
2022-01-07 00:34:25 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,10 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat [085713964963]
-│ • Gopay [085713964963]
+│ • Smartfren [088233832771]
+│ • Dana [088233832771]
 ╰────
 ╭─「 Hubungi 」
-│ > Ingin donasi? Wa.me/6281390658325
+│ > Ingin donasi? Wa.me/6288233832771
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
