0744c9bbe7e20d156b6c6c61927a2e042079ab09
games-wabot <97183746+Botwa021@users.noreply.github.com>
2022-01-08 15:57:10 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,13 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
 │ • Smartfren [088233832771]
+│ • Tri [089612698583]
 │ • Dana [088233832771]
 ╰────
 ╭─「 Hubungi 」
 │ > Ingin donasi? Wa.me/6288233832771
 ╰────
-`.trim()) // Tambah sendiri kalo mau
+`.trim()) // Makasih yang udah donasi
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
