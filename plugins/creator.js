7783bd5f01a5e9695b8695b288df756cea57f60b
games-wabot <97183746+Botwa021@users.noreply.github.com>
2022-01-08 14:28:51 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,5 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
+  // this.sendContact(m.chat, '6288233832771', '🐼🔥', m)
   let contacts = []
   for (let owner of Object.entries(global.Owner).filter(v => v[1].isCreator)) {
     contacts.push(...[owner[0], owner[1].name])
